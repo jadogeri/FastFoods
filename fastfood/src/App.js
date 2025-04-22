55cadbeee7ba892e685fe8bbb22f6fb1ef7b1932
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-04-22 15:51:40 -0500

    added assets to public directory

---
+++ fastfood/src/App.js
@@ -0,0 +1,12 @@
+import ProjectRoutes from './ProjectRoutes';
+import './App.css';
+
+function App() {
+  return (
+    <ProjectRoutes />
+
+
+  );
+}
+
+export default App;
---
+++ fastfood/src/ProjectRoutes.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+/**  ROUTES -- AUTHFLOW **/
+
+import Home from './pages/Home.jsx'
+import NoPage from "./pages/NoPage.jsx";
+
+
+const ProjectRoutes = () => {
+
+
+
+  return (
+
+
+	
+	<BrowserRouter >
+	{/* <NavBar />	 */}
+
+	{/* <AppBar/>	 */}
+		<Routes >				
+			<Route path="/" element={ <Home />} index /> 
+
+			<Route path="*" element={<NoPage />} />
+		</Routes>
+	</BrowserRouter>
+
+
+
+  )
+}
+
+
+
+export default ProjectRoutes
+
+
+
+
